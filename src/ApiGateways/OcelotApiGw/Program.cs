427852e6fc10e1af3d70e79488891f4093b21cac
Nilay Oyat <Nilay.Oyat@wealth-dynamix.com>
2023-08-13 09:09:53 +0100

    ,

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,18 +1,29 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true)
+                            .Build();
+
+builder.Services.AddOcelot(configuration)
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
+
 
 var app = builder.Build();
 
 
+await app.UseOcelot();
+
 app.MapGet("/", () => "Hello World!");
 
-await app.UseOcelot();
+
 
 app.Run();
---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtension
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong with api : {response.ReasonPhrase}");
+            }
+
+            var data = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(data, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+    }
+}
